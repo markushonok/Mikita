612164be20a1fffdb32f0143d8c10e91adc723b2
markushonok <nikita.markushonok@pix.ru>
2024-08-15 23:50:19 +0500

    Removed godot props

--- Godot/Subroutine.Props/ResourceProp.cs
+++
@@ -1,9 +0,0 @@
-using Godot;
-using Mikita.Subroutine.Props;
-
-namespace Mikita.Godot.Subroutine.Props;
-
-public abstract class ResourceProp<T> : Resource, ReadOnlyProp<T>
-	{
-		public abstract T Value { get; }
-	}
