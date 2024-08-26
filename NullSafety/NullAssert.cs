2ed554d192bd7cc52f6300867f13ba9839ca43b2
markushonok <nikita.markushonok@pix.ru>
2024-08-26 23:31:48 +0500

    Improved code in NullAssert.cs

--- NullSafety/NullAssert.cs
+++ NullSafety/NullAssert.cs
@@ -6,64 +6,91 @@ namespace Mikita.NullSafety;
 public static class NullAssert
 	{
 		/// <summary>
-		/// Asserts that <paramref name="object"/> is not null.
+		///		Asserts that <paramref name="value"/> is not null.
 		/// </summary>
 		/// 
-		/// <returns>Non-null <paramref name="object"/>.</returns>
+		/// <returns>Non-null <paramref name="value"/>.</returns>
 		///
 		/// <exception cref="ArgumentNullException">
-		/// Thrown if <paramref name="caller"/> is null.
-		/// </exception>
-		/// 
-		/// <exception cref="NullReferenceException">
-		/// Thrown if <paramref name="object"/> is null.
+		///		Thrown if <paramref name="value"/> or
+		///		<paramref name="caller"/> is null.
 		/// </exception>
 		[StackTraceHidden, DebuggerNonUserCode]
 		public static T NotNull<T>
 			(
-				this T? @object, 
+				this T? value,
 				[CallerMemberName] string? caller = null
-			) 
+			)
 			where T : class
 			{
-				if (caller is null)
-					throw new ArgumentNullException(NullCallerMessage<T>());
-				if (@object is null) 
-					throw new NullReferenceException(NullValueMessage<T>(caller));
-				return @object;
+				if (caller is null) throw NullCallerException<T>();
+				if (value  is null) throw NullValueException <T>(caller);
+				return value;
 			}
 
 		/// <summary>
-		/// Asserts that <paramref name="object"/> is not null.
+		///		Asserts that <paramref name="value"/> is not null.
 		/// </summary>
 		/// 
-		/// <returns>Non-null <paramref name="object"/>.</returns>
+		/// <returns>Non-null <paramref name="value"/>.</returns>
 		///
 		/// <exception cref="ArgumentNullException">
-		/// Thrown if <paramref name="caller"/> is null.
-		/// </exception>
-		/// 
-		/// <exception cref="NullReferenceException">
-		/// Thrown if <paramref name="object"/> is null.
+		///		Thrown if <paramref name="value"/> or
+		///		<paramref name="caller"/> is null.
 		/// </exception>
 		[StackTraceHidden, DebuggerNonUserCode]
 		public static T NotNull<T>
 			(
-				this T? @object, 
+				this T? value,
 				[CallerMemberName] string? caller = null
-			) 
+			)
 			where T : struct
 			{
-				if (caller is null)
-					throw new ArgumentNullException(NullCallerMessage<T>());
-        if (@object is null) 
-					throw new NullReferenceException(NullValueMessage<T>(caller));
-				return @object.Value;
+				if (caller is null) throw NullCallerException<T>();
+				if (value  is null) throw NullValueException <T>(caller);
+				return value.Value;
 			}
-		
-		private static string NullCallerMessage<T>() 
+
+		/// <typeparam name="T">The type of the caller.</typeparam>
+		///
+		/// <returns>
+		///		<see cref="ArgumentNullException"/> with a message that
+		///		describes that the caller cannot be null.
+		/// </returns>
+		[StackTraceHidden, DebuggerNonUserCode]
+		public static ArgumentNullException NullCallerException<T>()
+			=> new(NullCallerMessage<T>());
+
+		/// <typeparam name="T">The type of the caller.</typeparam>
+		/// 
+		/// <returns>
+		///		The string message that describes the <typeparamref name="T"/>
+		///		caller cannot be null.
+		/// </returns>
+		public static string NullCallerMessage<T>()
 			=> $"{nameof(T)} caller cannot be null.";
 
-		private static string NullValueMessage<T>(string caller) 
+		/// <typeparam name="T">The type of the value.</typeparam>
+		/// 
+		/// <returns>
+		///		<see cref="ArgumentNullException"/> with a message that describes
+		///		that the value cannot be null.
+		/// </returns>
+		public static ArgumentNullException NullValueException<T>
+			(
+				string caller
+			)
+			=> new(NullValueMessage<T>(caller));
+
+		/// <param name="caller">The name of the caller.</param>
+		/// <typeparam name="T">The type of the caller.</typeparam>
+		/// <returns>
+		///		The string message that describes the <typeparamref name="T"/>
+		///		<paramref name="caller"/> asserted to be non-null but it's null.
+		/// </returns>
+		public static string NullValueMessage<T>
+			(
+				string caller
+			)
 			=> $"{nameof(T)} {caller} asserted to be non-null but it's null.";
 	}
