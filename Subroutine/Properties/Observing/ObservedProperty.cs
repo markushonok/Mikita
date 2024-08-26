ddb089942ad27f0a361e90c117caad46926bb6fe
markushonok <nikita.markushonok@pix.ru>
2024-08-26 23:34:23 +0500

    Fix format in ObservedProperty.cs

--- Subroutine/Properties/Observing/ObservedProperty.cs
+++ Subroutine/Properties/Observing/ObservedProperty.cs
@@ -2,6 +2,7 @@ using Mikita.Subroutine.Observing;
 
 namespace Mikita.Subroutine.Properties.Observing;
 
-public interface ObservedProperty<T> : 
-	Property<T>,
-	ObservedChange;
+public interface ObservedProperty<T> 
+	: 
+		Property<T>, 
+		ObservedChange;
