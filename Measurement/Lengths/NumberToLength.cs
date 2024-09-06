b58af39541fd00ad9d9f2814ce458af2abb1e8d8
markushonok <nikita.markushonok@pix.ru>
2024-09-07 02:29:05 +0500

    Add meters literal

--- Measurement/Lengths/NumberToLength.cs
+++ Measurement/Lengths/NumberToLength.cs
@@ -7,6 +7,10 @@ public static class NumberToLength
 		public static LengthRecord<T> km<T>(this T number)
 			where T : INumber<T>, IMultiplyOperators<T, int, T>
 			=> new(m: number * 1000);
+		
+		public static LengthRecord<T> m<T>(this T number)
+			where T : INumber<T>, IDivisionOperators<T, int, T>
+			=> new(m: number);
 
 		public static LengthRecord<T> cm<T>(this T number)
 			where T : INumber<T>, IDivisionOperators<T, int, T>
