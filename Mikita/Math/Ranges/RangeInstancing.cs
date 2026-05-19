8dc5d2e13d7fa6d7e4f8ad4301e633b7db782164
Никита Маркушонок <4Co_dev@mail.ru>
2026-05-19 14:28:34 +0500

    Add `Range` property for instancing ranges with min and max values

--- Mikita/Math/Ranges/RangeInstancing.cs
+++ Mikita/Math/Ranges/RangeInstancing.cs
@@ -1,7 +1,16 @@
+using System.Numerics;
+
 namespace Mikita.Math.Ranges;
 
 public static class RangeInstancing
 	{
+		extension<T>(T value)
+			where T: IMinMaxValue<T>
+			{
+				public static IRange<T> Range
+					=> Range.Between(T.MinValue, T.MaxValue);
+			}
+
 		extension(Range)
 			{
 				public static Range<T> Between<T>
