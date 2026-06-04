1675935d73c8d025e32bf6dfa730746b67dfbe98
Никита Маркушонок <4Co_dev@mail.ru>
2026-06-04 16:29:00 +0500

    Add `ValueAt` method to `TomlPathAccess` for retrieving values by path

--- Mikita/FileSystems/Files/Toml/TomlPathAccess.cs
+++ Mikita/FileSystems/Files/Toml/TomlPathAccess.cs
@@ -16,6 +16,13 @@ public static class TomlPathAccess
 						return new TomlRef<T>(table, property);
 					}
 
+				public T ValueAt<T>(IPath path)
+					{
+						var table = root.SubTableAt(path.HigherBy(1));
+						var property = path.Elements[^1];
+						return (T) table[property];
+					}
+
 				public TomlTable SubTableAt(IPath path)
 					=> path.Elements.Aggregate
 						(
