b85c9effc32952af52c71ca5cd3f8dbd516603db
Никита Маркушонок <4Co_dev@mail.ru>
2026-06-25 20:40:32 +0500

    Add `AsSafeTomlIO` and enhanced `AsTomlIO` overloads to improve TOML file instance safety and default handling.

--- Mikita/FileSystems/Files/Toml/TomlFileInstancing.cs
+++ Mikita/FileSystems/Files/Toml/TomlFileInstancing.cs
@@ -1,10 +1,26 @@
+using System;
+using Tomlyn.Model;
+
 namespace Mikita.FileSystems.Files.Toml;
 
 public static class TomlFileInstancing
 	{
 		extension(IFile file)
 			{
+				public IFileIO<ITomlTable> AsSafeTomlIO
+					=> file.AsTomlIO().WithDefault
+						(
+							file,
+							value: () => new TomlTable().AsAbstract
+						);
+
 				public IFileIO<ITomlTable> AsTomlIO
+					(
+						Func<ITomlTable> @default
+					)
+					=> new TomlFileIO(file).WithDefault(file, @default);
+
+				public IFileIO<ITomlTable> AsTomlIO()
 					=> new TomlFileIO(file);
 			}
 	}
