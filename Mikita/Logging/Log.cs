f2e4664f9166ec6a70b06013e2fbdc944fd54b64
Никита Маркушонок <4Co_dev@mail.ru>
2026-08-03 15:37:45 +0500

    Move logging creation into Log class

--- Mikita/Logging/Log.cs
+++ Mikita/Logging/Log.cs
@@ -1,3 +1,15 @@
+using System.Collections.Generic;
+
 namespace Mikita.Logging;
 
-public static class Log;
+public static class Log
+	{
+		public static ILog Debug
+			=> DebugLog.Instance;
+
+		public static MultiLog With
+			(
+				IEnumerable<ILog> logs
+			)
+			=> new(logs);
+	}
